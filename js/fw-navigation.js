598c67e2aa5203b4fc9a57b4fa67badb53b44c3b
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2016-02-25 03:50:34 -0500

    refactor(css,js): use data-states instead of classes to toggle nav

--- js/fw-navigation.js
+++ js/fw-navigation.js
@@ -2,7 +2,16 @@ $(function(){
   $('body').removeClass('noscript');
 
   $('.fw-toggle').click(function(e){
-    e.preventDefault();
-    $(this).toggleClass('on');
+    e.preventDefault()
+    toggleNav();
   });
 });
+
+function toggleNav(){
+  if($('.fw-header').attr('data-state') == 'closed'){
+    $('.fw-header').attr('data-state','open');
+  }else{
+    $('.fw-header').attr('data-state','closed');
+  }
+  console.log('test');
+}
