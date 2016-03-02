d9ac062c2b1ed6aecf382fe70f06f56560dcf918
natalie-macpherson <macphersonnatalie@gmail.com>
2016-03-02 14:38:52 -0500

    feat(js/sass): compressed js and css files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//@prepros-append countdown-clock.js
+//@prepros-append filter.js
+//@prepros-append fw-navigation.js
+//@prepros-append instafeed.min.js
+//@prepros-append instagram-feed.js
+//@prepros-append jquery.waypoints.min.js
+//@prepros-append scroll.js
+//@prepros-append video.js
