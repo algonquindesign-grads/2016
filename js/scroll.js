d4fab8fd86a02c7a1ba3077a2af131cb94280c57
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2016-03-06 01:56:59 -0500

    refactor(js): fix scrollTop offsets

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,28 @@
 var $rtt = $('.rtt');
 
-$('.fw-nav-list').localScroll();
-$('.rtt').localScroll();
+// media query event handler
+if (matchMedia) {
+  var mq = window.matchMedia("(min-width: 80em)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+
+  if (mq.matches) {
+    $('.fw-nav-list').localScroll({offset: { top: 0 }});
+    $('.rtt').localScroll({offset: { top: -100 }});
+  } else {
+    $('.fw-nav-list').localScroll({offset: { top: -80 }});
+    $('.rtt').localScroll({offset: { top: -100 }});
+  }
+
+}
+
+
+
+
 
 var $ah = $('.about-holder');
 
